f53d336c90fd3361d5c9b67f561185f2d096e615
VictorEspancaGravida <angelovictor642@gmail.com>
2024-03-21 22:10:21 -0300

    Finalizado

---
+++ sinval-main/Resposividade/script.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    var itensMenu = document.getElementById('itens')
+    if (itensMenu.style.display == 'block') {
+        itensMenu.style.display = 'none'
+    } else {
+        itensMenu.style.display = 'block'
+    }
+}
